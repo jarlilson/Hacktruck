98c109fc8a315a038c008862ae1beedc7d2f30c9
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2023-03-02 12:03:35 -0300

    Add files via upload

---
+++ Rota 1 - Algoritmos/1.1 - Conceitos Iniciais/bool(test).swift
@@ -0,0 +1,15 @@
+//Testando possibilidades booleanas
+
+print(1 > 2)
+
+print(1 == 2)
+
+print(1 != 2)
+
+print(1 <= 2 && false) //true && false
+
+print(1 < 2 && true) //true && true
+
+print(2 < 2 || true) //false || true
+
+print(2 <= 2 && true) //true && true
