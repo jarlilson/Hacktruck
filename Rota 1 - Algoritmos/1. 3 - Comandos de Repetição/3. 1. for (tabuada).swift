a8b499d9e398fdd6fae3b28411d03ed2282b5454
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2023-03-06 15:34:30 -0300

    Update 3. 1. for (tabuada).swift

--- Rota 1 - Algoritmos/1. 3 - Comandos de Repetição/3. 1. for (tabuada).swift
+++ Rota 1 - Algoritmos/1. 3 - Comandos de Repetição/3. 1. for (tabuada).swift
@@ -1,8 +1,8 @@
 //Criação da tabuada do 7
 
-var i = 1
+var i = 1 //DESNECESSÁRIO
 
 for i in 1...10 {
-    let multiplica = 7 * i
+    let multiplica = 7 * i //PODE SER ADAPTADO
     print("7 x \(i) = \(multiplica)")
 }
