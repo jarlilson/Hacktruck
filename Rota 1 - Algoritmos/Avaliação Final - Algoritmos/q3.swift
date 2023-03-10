4b2c61a44e632460f76f1ff947ec07b9f9d19315
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2023-03-10 09:47:37 -0300

    Add files via upload

---
+++ Rota 1 - Algoritmos/Avaliação Final - Algoritmos/q3.swift
@@ -0,0 +1,19 @@
+//Avaliação Final - Algoritmos ((do curso HackaTruck MakerSpace))
+//Questão 03:
+
+var texto: String;
+
+texto = "amarelo"
+
+switch texto {
+    case "vermelho":
+        print("PARE!")
+    case "amarelo":
+        print("ATENÇÃO, VAI FECHAR")
+    case "verde":
+        print("SIGA EM FRENTE")
+    case "piscando":
+        print("PARE E SIGA EM FRENTE")
+default:
+    print("SEMÁFORO DESLIGADO")
+}
