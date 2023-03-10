cd92083d58b440cf8834d10b209bde4ef815909e
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2023-03-10 10:15:58 -0300

    Add files via upload

---
+++ Rota 1 - Algoritmos/Avaliação Final - Algoritmos/q10.swift
@@ -0,0 +1,10 @@
+//Avaliação Final - Algoritmos ((do curso HackaTruck MakerSpace))
+//Questão 10:
+
+var a, b, c : Int;
+
+a = 2;
+b = 3;
+c = (b+a) * b;
+
+print(c)
---
+++ Rota 1 - Algoritmos/Avaliação Final - Algoritmos/q9.swift
@@ -0,0 +1,26 @@
+//Avaliação Final - Algoritmos ((do curso HackaTruck MakerSpace))
+//Questão 09:
+
+var numero: Int;
+
+numero = 110;
+
+if (numero < 0) {
+    print("Saída 1")
+}
+
+if (numero > 0 && (numero < 10)) {
+    print("Saída 2")
+}
+
+if (numero >= 10 && (numero < 100)) {
+    print("Saída 3")
+}
+
+if (numero >= 100 && (numero < 110)) {
+    print("Saída 4")
+}
+
+if (numero >= 110) {
+    print("Saída 5")
+}
