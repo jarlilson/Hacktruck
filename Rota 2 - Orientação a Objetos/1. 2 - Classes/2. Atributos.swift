6424f58ff68e30dba95223e5140b60d242e08e97
Jarlilson Guajajara <73809633+jarlilson@users.noreply.github.com>
2023-03-13 18:37:31 -0300

    Add files via upload

---
+++ Rota 2 - Orientação a Objetos/1. 2 - Classes/2. Atributos.swift
@@ -0,0 +1,12 @@
+// Criem uma classe chamada Musica, e adicionem os atributos que vocês imaginam para música.
+
+class Musica{
+    var compositor: String?
+    var campoHarmonico: String?
+    var nome: String?
+    var anoLancamento: Int?
+    var duracao: Double?
+    var rating: Int?
+    var linkToPlay: String?
+    
+}
